70b569ccb97efaffb03721d32efd41477a053ee5
rhidasi <robert.hidasi@gmail.com>
2018-02-17 14:03:17 +0100

    Revert unwanted change

--- app/src/main/java/sk/hidasi/balance_tr/BalanceWidget.java
+++ app/src/main/java/sk/hidasi/balance_tr/BalanceWidget.java
@@ -53,6 +53,7 @@ public class BalanceWidget extends AppWidgetProvider {
 
 	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+		// There may be multiple widgets active, so update all of them
 		for (int appWidgetId : appWidgetIds) {
 			BalanceWidgetHelper.createHttpRequest(context, appWidgetManager, appWidgetId);
 		}
